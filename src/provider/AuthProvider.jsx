8d63b4cc81539fac03497419ba73b1cb4854c1c2
lijanahmmed <lijanahmmed@gmail.com>
2025-12-12 21:45:35 +0600

    added email password auth in firebase

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,19 +2,24 @@ import React, { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
 export const AuthContext = createContext();
 import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
   updateProfile,
 } from "firebase/auth";
 
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -28,6 +33,15 @@ const AuthProvider = ({ children }) => {
 
   const updateUser = (updatedData) => {
     return updateProfile(auth.currentUser, updatedData);
+  };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const signInWithGithub = () => {
+    setLoading(true);
+    return signInWithPopup(auth, githubProvider);
   };
 
   const logOut = () => {
@@ -53,6 +67,8 @@ const AuthProvider = ({ children }) => {
     loading,
     setLoading,
     updateUser,
+    signInWithGoogle,
+    signInWithGithub,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
